5d37292e0fbbe9446ce55fd9e6108608af80b11f
James <james@jennens.co.uk>
2020-02-02 14:49:55 -0500

    removed single quote

--- simulation_ws/src/rl-agent/markov/environments/mars_env.py
+++ simulation_ws/src/rl-agent/markov/environments/mars_env.py
@@ -407,7 +407,7 @@ class MarsEnv(gym.Env):
                 return 0, True # No reward
             
             if abs(self.x - self.last_position_x) + abs(self.y-self.last_position_y) < 0.001:
-		print("Rover hasn't moved")
+		print("Rover has not moved")
 		return 0, True
 
             # Has the rover reached the max steps
